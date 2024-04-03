7998e992ce6aeb8c0ede1c6b3c0de718c9a8dbbc
Jakub Olejnik <jakub.olejnik@ackee.cz>
2024-04-03 17:38:01 +0200

    Make code uncompilable

--- PragueOAPISwiftUI/AppDelegate.swift
+++ PragueOAPISwiftUI/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     ) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = RootViewController()
+        window?.raaaaootViewController = RootViewController()
         window?.makeKeyAndVisible()
         
         return true
